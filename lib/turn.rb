32bfef8cf7f1b0df7b86bd4f7681fccb30470ae6
Drew Thompson <drewallynthompson@gmail.com>
2020-08-23 22:28:19 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -33,7 +33,7 @@ def turn(board)
    input = gets.strip
    index = input_to_index(input)
    if valid_move?(board, index)
-      move(board, index, player(board))
+      move(board, index, player
       turn(board)
    end
    display_board(board)
